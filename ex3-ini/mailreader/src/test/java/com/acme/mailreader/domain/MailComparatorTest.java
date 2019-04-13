15661141b6c1b924e2370d672a6d9c9a9fb768ca
godymcfly <thibaut.godet@protonmail.com>
2019-04-13 14:02:03 +0200

    Fin exo3

--- ex3-ini/mailreader/src/test/java/com/acme/mailreader/domain/MailComparatorTest.java
+++ ex3-ini/mailreader/src/test/java/com/acme/mailreader/domain/MailComparatorTest.java
@@ -3,11 +3,14 @@ package com.acme.mailreader.domain;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+import java.time.Instant;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import com.acme.mailreader.domain.Mail;
 import com.acme.mailreader.domain.MailComparator;
+import com.acme.mailreader.domain.Mail.Statut;
 
 public class MailComparatorTest {
 	
@@ -32,9 +35,39 @@ public class MailComparatorTest {
 		assertThat(comparator.compare(mail1, mail2), is(0));
 	}
 	
-	//TODO
-	//Autres tests unitaires
+	@Test 
+	public final void egauxSiMailsEgaux() throws DateIncorrecteException {
+		Mail mail1 = new Mail.Builder("uyyuy").important(true).statut(Statut.READ).date(Instant.now()).build();
+		Mail mail2 = new Mail.Builder("uyyuy").important(true).statut(Statut.READ).date(Instant.now()).build();
+		assertThat(comparator.compare(mail1, mail2),is(0));	
+	}	
 	
+	@Test
+	public final void inferieurSiDateNulle() throws DateIncorrecteException  {
+		Mail mail1 = new Mail.Builder("uyyuy").important(false).statut(Statut.READ).build();
+		Mail mail2 = new Mail.Builder("uyyuy").important(false).statut(Statut.READ).date(Instant.now()).build();
+		assertThat(comparator.compare(mail1, mail2),is(1));
+	}
 	
+	@Test 
+	public final void inferieurSiSujetNul() throws DateIncorrecteException {
+		Mail mail1 = new Mail.Builder(null).important(false).statut(Statut.READ).date(Instant.now()).build();
+		Mail mail2 = new Mail.Builder("uyyuy").important(false).statut(Statut.READ).date(Instant.now()).build();
+		assertThat(comparator.compare(mail1, mail2),is(1));	
+	}
+	
+	@Test 
+	public final void inferieurSiImportanceFalse() throws DateIncorrecteException {
+		Mail mail1 = new Mail.Builder("uyyuy").important(false).statut(Statut.READ).date(Instant.now()).build();
+		Mail mail2 = new Mail.Builder("uyyuy").important(true).statut(Statut.READ).date(Instant.now()).build();
+		assertThat(comparator.compare(mail1, mail2),is(1));	
+	}
+
+	@Test
+	public final void mailDateRecenteEnPremier() throws DateIncorrecteException {
+		Mail mail1 = new Mail.Builder("sujet").date(Instant.now()).build();
+		Mail mail2 = new Mail.Builder("sujet").date(Instant.now().plusSeconds(360000)).build();
+		assertThat(comparator.compare(mail1, mail2), is(MailComparator.PREMIER_PLUS_GRAND));		
+} 
 	
 }
--- ex3-ini/mailreader/src/test/java/com/acme/mailreader/domain/MailTest.java
+++ ex3-ini/mailreader/src/test/java/com/acme/mailreader/domain/MailTest.java
@@ -8,12 +8,12 @@ import java.time.Instant;
 import org.junit.Test;
 
 import com.acme.mailreader.domain.Mail.Statut;
+import com.acme.mailreader.presentation.MailInvalideException;
 
 public class MailTest {
 	
 	MailComparator comparator = new MailComparator();
 	
-
 	@Test(expected=DateIncorrecteException.class)
 	public final void erreurSiDateAvant1970() throws DateIncorrecteException {
 					
@@ -24,7 +24,25 @@ public class MailTest {
 		Mail mail1 = new Mail.Builder("uyyuy").important(false).statut(Statut.READ).build();
 		Mail mail2 = new Mail.Builder("uyyuy").important(false).statut(Statut.READ).date(Instant.now()).build();
 		assertThat(comparator.compare(mail1, mail2),is(1));
-				
 	}
+
+	@Test(expected=MailInvalideException.class)
+	public final void sujetTropLong() throws MailInvalideException {
+		Mail mail1 = new Mail.Builder("Sujet trop long, Sujet trop long, Sujet trop long, Sujet trop long, Sujet trop long, Sujet trop long, Sujet trop long, Sujet trop long, Sujet trop long, Sujet trop long, Sujet trop long, Sujet trop long, ").build();
+	}
+	
+	@Test
+	public final void mailsEgaux() throws DateIncorrecteException {
+		Mail mail1 = new Mail.Builder("uyyuy").important(false).statut(Statut.READ).date(Instant.now()).build();
+		Mail mail2 = new Mail.Builder("uyyuy").important(false).statut(Statut.READ).date(Instant.now()).build();
+		assertThat(mail1.equals(mail2),is(true));	
+	}
+	
+	@Test
+	public final void mailsInegaux() throws DateIncorrecteException {
+		Mail mail1 = new Mail.Builder("JOUOJOU").important(false).statut(Statut.READ).date(Instant.now()).build();
+		Mail mail2 = new Mail.Builder("uyyuy").important(false).statut(Statut.READ).date(Instant.now()).build();
+		assertThat(mail1.equals(mail2),is(false));	
+}
 
 }
